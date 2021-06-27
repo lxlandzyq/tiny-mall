7875604abf01ab9099a83ac212fdd30afe6cd172
lxlandzyq <1391889724@qq.com>
2021-06-27 09:43:33 +0800

    添加swagger的配置

---
+++ src/main/java/com/lxl/tiny/mall/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.lxl.tiny.mall.config;
+
+import com.lxl.tiny.mall.common.config.BaseSwaggerConfig;
+import com.lxl.tiny.mall.common.domain.SwaggerProperties;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger API文档相关配置
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends BaseSwaggerConfig {
+    @Override
+    public SwaggerProperties swaggerProperties() {
+        return SwaggerProperties.builder()
+                .apiBasePackage("com.lxl.")
+                .title("tiny-mall项目骨架")
+                .description("tiny-mall项目骨架相关接口文档")
+                .contactName("lxl")
+                .version("1.0")
+                .enableSecurity(true)
+                .build();
+    }
+}
